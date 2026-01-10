e45512091867e850b2ab4f91d5dcceff66a91a1c
Komodo <45665554+Komodo5197@users.noreply.github.com>
2026-01-10 15:21:36 -0500

    Update build_windows.sh

--- tool/build_windows.sh
+++ tool/build_windows.sh
@@ -3,7 +3,11 @@ if [ "$1" = "x64" ]; then
   cargo build --target x86_64-pc-windows-msvc --release
   mv "target/x86_64-pc-windows-msvc/release/isar.dll" "isar_windows_x64.dll"
 else
-  rustup target add aarch64-pc-windows-msvc
-  cargo build --target aarch64-pc-windows-msvc --release
+  echo "Building for Windows arm64 with fixed Rust 1.88.0"
+  RUST_TOOLCHAIN="1.88.0-aarch64-pc-windows-msvc"
+  rustup target add aarch64-pc-windows-msvc --toolchain $RUST_TOOLCHAIN
+  rustup run $RUST_TOOLCHAIN rustc --version
+  rustup run $RUST_TOOLCHAIN cargo --version
+  rustup run $RUST_TOOLCHAIN cargo build --target aarch64-pc-windows-msvc --release
   mv "target/aarch64-pc-windows-msvc/release/isar.dll" "isar_windows_arm64.dll"
 fi
